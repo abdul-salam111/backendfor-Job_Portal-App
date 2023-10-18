f1f75c7d5cfc3159f0a56a093b70c79c9c19b80f
Abdul Salam <you@example.com>
2023-10-18 15:11:26 +0500

    hello

--- models/user.js
+++ models/user.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema(
         location: { type: String, required: false },
         isAdmin: { type: Boolean, default: true },
         isAgent: { type: Boolean, default: false },
-        skills: { type: Array, default: false },
+        skills: { type: Array, default: [] },
         profile: { type: String, required: true, 
             default: "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_640.png" },
 
