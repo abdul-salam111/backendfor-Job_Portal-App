b9ec4061dbb21675ba9a40c247cb8ac93fe880ec
Abdul Salam <you@example.com>
2023-10-24 16:04:25 +0500

    hello

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -44,7 +44,7 @@ module.exports = {
     getAllJobs: async (req, res) => {
         try {
             const allJobs = await Job.find();
-            res.status(200).json(allJobs);
+            res.status(200).json({allJobs});
         } catch (error) {
             console.error(error);
             res.status(500).json({ "message": "Internal server error" });
